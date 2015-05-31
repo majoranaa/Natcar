17def7358dde5cc11f136e51442a073b30fdd914
kai yang <doublek200@gmail.com>
2015-05-30 17:13:23 -0700

    asdf

--- params.h
+++ params.h
@@ -16,6 +16,8 @@
 #define OFFSET       20     // DEPRECATED; When Natcar doesn't see the track anymore, turn OFFSET degrees from the MIDPNT in the direction of the previously seen maximum
 #define MIDPNT       100    // The experimentally determined midpoint of the servo (ideally 90deg)
 #define MIDDLE       64     // The index of the center of the track in a sample from the linescan camera
+#define MAX_SPEED    170    // Maximum speed (0-255) that car can go
+#define MIN_SPEED    130    // Minimum speed (0-255) that car can go
 #define THRESH       100    // Number of iterations of loop() that the car will go without seeing the track until it stops (assuming DEBUG_STOP is defined)
 #define UP_THRESH    21     // The maximum error (distance of track from MIDDLE) allowed under which the car will accelerate at rate ACCEL
 #define DOWN_THRESH  22     // The minimum error required above which the car will decelerate at rate DECEL
@@ -34,6 +36,7 @@
 #define WIDTH_THRESH 25
 
 #define K_P          .8//1
+#define K_D          0.04 
 #define K_I          0.2
 
 #endif // PARAMS_H
